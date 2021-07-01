dc8c54976e63c4ab3ba20c8514d890162924a577
Azraei Yusof <mazei513@gmail.com>
2021-07-01 13:52:16 +0800

    Added spend method, argument guards

--- pointsbook.go
+++ pointsbook.go
@@ -1,12 +1,17 @@
 package pointsbook
 
+import "errors"
+
 type Book struct {
 	id   string
 	trxs []int
 }
 
-func NewBook(id string) *Book {
-	return &Book{id: id}
+func NewBook(id string) (*Book, error) {
+	if id == "" {
+		return nil, errors.New("empty ID")
+	}
+	return &Book{id: id}, nil
 }
 
 func (b *Book) ID() string { return b.id }
@@ -19,8 +24,17 @@ func (b *Book) CurrentPoints() int {
 	return p
 }
 
-func (b *Book) AddPoints(p int) {
-	b.trxs = append(b.trxs, p)
+func (b *Book) AddPoints(p uint) {
+	b.trxs = append(b.trxs, int(p))
 }
 
 func (b *Book) Transactions() []int { return b.trxs }
+
+func (b *Book) Spend(p uint) (ok bool) {
+	pi := int(p)
+	if b.CurrentPoints() < pi {
+		return false
+	}
+	b.trxs = append(b.trxs, -1*pi)
+	return true
+}
--- pointsbook_test.go
+++ pointsbook_test.go
@@ -8,14 +8,26 @@ import (
 )
 
 func TestNewBook(t *testing.T) {
-	b := pointsbook.NewBook("book-id")
+	t.Run("normal ID", func(t *testing.T) {
+		b, err := pointsbook.NewBook("book-id")
 
-	assertInitialBookState(t, b, "book-id")
+		assertInitialBookState(t, b, err, "book-id")
+	})
+	t.Run("empty ID", func(t *testing.T) {
+		b, err := pointsbook.NewBook("")
+
+		if err == nil {
+			t.Fatal("expected error, got none")
+		}
+		if b != nil {
+			t.Fatal("expected nil Book, got non-nil")
+		}
+	})
 }
 
 func TestBookAddPoints(t *testing.T) {
-	b := pointsbook.NewBook("book-1")
-	assertInitialBookState(t, b, "book-1")
+	b, err := pointsbook.NewBook("book-1")
+	assertInitialBookState(t, b, err, "book-1")
 
 	b.AddPoints(1)
 
@@ -27,8 +39,11 @@ func TestBookAddPoints(t *testing.T) {
 	}
 }
 
-func assertInitialBookState(t *testing.T, b *pointsbook.Book, id string) {
+func assertInitialBookState(t *testing.T, b *pointsbook.Book, err error, id string) {
 	t.Helper()
+	if err != nil {
+		t.Fatalf("expected no error, got %s", err.Error())
+	}
 	if b.ID() != id {
 		t.Fatalf("expected id %s, got %s", strconv.Quote(id), strconv.Quote(b.ID()))
 	}
@@ -41,13 +56,38 @@ func assertInitialBookState(t *testing.T, b *pointsbook.Book, id string) {
 }
 
 func BenchmarkCurrentPoints(b *testing.B) {
-	bb := pointsbook.NewBook("bench-book")
+	bb, _ := pointsbook.NewBook("bench-book")
 	for i := 0; i < 1000; i++ {
-		bb.AddPoints(i)
+		bb.AddPoints(uint(i))
 	}
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		bb.CurrentPoints()
 	}
 	b.StopTimer()
+}
+
+func TestBookSpendPoints(t *testing.T) {
+	b, err := pointsbook.NewBook("book-b")
+	assertInitialBookState(t, b, err, "book-b")
+	b.AddPoints(10)
+	assertBookCurrentPoints(t, b, 10)
+
+	ok := b.Spend(5)
+	if !ok {
+		t.Fatalf("expected ok on spend, got not ok")
+	}
+	assertBookCurrentPoints(t, b, 5)
+
+	ok = b.Spend(6)
+	if ok {
+		t.Fatalf("expected not ok on spend, got ok")
+	}
+	assertBookCurrentPoints(t, b, 5)
+}
+
+func assertBookCurrentPoints(t *testing.T, b *pointsbook.Book, p int) {
+	if b.CurrentPoints() != p {
+		t.Fatalf("expected %d points, got %d", p, b.CurrentPoints())
+	}
 }
